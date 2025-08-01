0627b64bab936c3a29ecb6d5fe76c796ed66e59c
Le-Hoai-Huan <149983414+Le-Hoai-Huan@users.noreply.github.com>
2025-08-01 20:36:52 +0700

    user and auth

---
+++ BEQuestionBank.Domain/Interfaces/Service/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using BEQuestionBank.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BEQuestionBank.Domain.Interfaces.Service
+{
+    public interface IUserService : IService<NguoiDung>
+    {
+        Task<NguoiDung> GetByIdAsync(Guid maNguoiDung); 
+        Task<NguoiDung> CreateAsync(NguoiDung user);   
+        Task<NguoiDung> UpdateAsync(Guid maNguoiDung, NguoiDung user); 
+        Task<bool> DeleteAsync(Guid maNguoiDung);
+        Task<NguoiDung> GetByUsernameAsync(string tenDangNhap);
+    }
+}
